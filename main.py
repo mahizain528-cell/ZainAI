a3f23a3e5374d511f940910a9b5a57f2f13fefdd
Zain <mahizain528@gmail.com>
2026-09-19 22:01:29 -0700

    Add environment configuration

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-print("Hello! I am Zain AI.")
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+assistant_name = os.getenv("ASSISTANT_NAME")
+user_name = os.getenv("USER_NAME")
+
+print(f"Hello {user_name}!")
+print(f"My name is {assistant_name}.")
 print("Your personal AI assistant is starting...")
